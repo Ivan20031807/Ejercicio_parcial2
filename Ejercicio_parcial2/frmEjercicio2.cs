6f23dea2675116b33ab16610a2ceb0dee9bc4846
estudiante <estudiante@L-P1A-111>
2021-12-06 12:13:30 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_parcial2/frmEjercicio2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_parcial2
+{
+    public partial class frmEjercicio02 : Form
+    {
+        public frmEjercicio02()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            //valido si el usuario ingresó una frase
+            if (this.txtFrase.Text.Length == 0)
+            {
+                MessageBox.Show("Por favor ingrese una frase...");
+                //ubica el cursor en el cuaderno de texto txtfrase
+                this.txtFrase.Focus();
+            }
+            //creo el array de palabras
+            string[] palabras = this.txtFrase.Text.Split(' ');
+            this.lstPalabras.Items.Clear();
+
+            //lleno el listbox con las palabras
+            for (int i = 0; i < palabras.Length; i++)
+            {
+                this.lstPalabras.Items.Add(palabras[i] + "-->"+ palabras[i].Length +"caracteres...");
+            }
+        }
+    }
+}
